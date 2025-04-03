1c99f3b1ed3b534dafeacf947484e73dfc774056
Thormes Filgueira Leite Pereira <thormes@gmail.com>
2025-04-03 15:00:25 -0300

    refactor: :lipstick: Atualização do globals.css para adicionar as cores do figma. Colocar fonte Inter como padrão da aplicação
    Alteração do arquivo globals.css para adicionar ao tema as cores constantes do figma. Colocar como padrão na aplicação a fonte Inter

--- src/app/(private)/layout.js
+++ src/app/(private)/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "../globals.css";
 import HeaderBase from "@/components/layout/header-base";
 
@@ -6,6 +6,11 @@ const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: "--font-inter"
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -20,7 +25,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>
+      <body className={`${inter.className}`}>
         <HeaderBase/>
         {children}
       </body>
